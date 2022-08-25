6b211903e54f61364055f36ec47c899650f438aa
Sobhan <sobhanyazdanjo@gmail.com>
2022-08-26 02:55:03 +0430

    feat: Add router and token to browser after logging

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React, { FormEvent, useEffect, useState } from "react";
-import { getData } from "./firebase/transportLayer";
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
+import Home from "./components/Login/Home/Home";
+import "./App.css";
 
 function App() {
-  return <Login />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,6 @@
-import React, { FormEvent, useEffect, useState } from "react";
+import React, { FormEvent, useEffect, useLayoutEffect, useState } from "react";
 import { getData } from "../../firebase/transportLayer";
+import { useNavigate } from "react-router-dom";
 import "./login.css";
 
 const Login = () => {
@@ -13,53 +14,59 @@ const Login = () => {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [users, setUsers] = useState<User[]>([]);
+  let navigate = useNavigate();
 
   useEffect(() => {
-    getData().then((data: any) => setUsers(data));
+    if (!window.localStorage.getItem("token")) {
+      getData().then((data: any) => setUsers(data));
+    } else navigate("/");
   }, []);
 
   const loginFormSubmitHandler = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(
-      users.find(
-        (user) => user.fullname === userName && user.password === password
-      ) === undefined
-        ? "not authorized"
-        : "authorized"
+    let authenticated = users.find(
+      (user) => user.fullname === userName && user.password === password
     );
+    console.log("state of user", authenticated);
+    if (authenticated !== undefined) {
+      navigate("/");
+      window.localStorage.setItem("token", authenticated.id);
+    }
   };
 
   return (
     <div className="container">
-      <div className="text">Login</div>
-      <form onSubmit={(e) => loginFormSubmitHandler(e)}>
-        <div className="user-input-wrp">
-          <br />
-          <input
-            type="text"
-            className="inputText"
-            required
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-          />
-          <label className="floating-label">Your username</label>
-        </div>
+      <div className="login">
+        <div className="text">Login</div>
+        <form onSubmit={(e) => loginFormSubmitHandler(e)}>
+          <div className="user-input-wrp">
+            <br />
+            <input
+              type="text"
+              className="inputText"
+              required
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+            <label className="floating-label">Your username</label>
+          </div>
 
-        <div className="user-input-wrp">
-          <br />
-          <input
-            type="password"
-            className="inputText"
-            required
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <label className="floating-label">Your password</label>
-        </div>
+          <div className="user-input-wrp">
+            <br />
+            <input
+              type="password"
+              className="inputText"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <label className="floating-label">Your password</label>
+          </div>
 
-        <div className="forgot-pass"></div>
-        <button type="submit">Sign in</button>
-      </form>
+          <div className="forgot-pass"></div>
+          <button type="submit">Sign in</button>
+        </form>
+      </div>
     </div>
   );
 };
