7bbc6eff205142c972a4304f0b0b828ad4b103b7
Sobhan <sobhanyazdanjo@gmail.com>
2022-08-28 13:41:35 +0430

    style: Add styling to table buttons

--- src/components/Home/Table/Table.tsx
+++ src/components/Home/Table/Table.tsx
@@ -11,6 +11,7 @@ const Table = () => {
   }
   const [users, setUsers] = useState<User[]>([]);
   const [arrow, setArrow] = useState(false);
+  const [selectedUser, setSelectedUser] = useState<User>();
 
   useEffect(() => {
     getData().then((data: any) => setUsers(data));
@@ -30,31 +31,43 @@ const Table = () => {
     });
   };
 
+  const userClickedHandler = (user: User) => {
+    setSelectedUser(user);
+  };
+
   return (
-    <div className="table-container">
-      <table>
-        <thead>
-          <tr>
-            <th onClick={nameSortClickedHandler}>
-              Name
-              <div className={`arrow-icon ${arrow ? "" : "open"}`}>
-                <span className="left-bar"></span>
-                <span className="right-bar"></span>
-              </div>
-            </th>
-            <th>Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <th> {user.fullname} </th>
-              <td> {user.description} </td>
+    <>
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th onClick={nameSortClickedHandler}>
+                Name
+                <div className={`arrow-icon ${arrow ? "" : "open"}`}>
+                  <span className="left-bar"></span>
+                  <span className="right-bar"></span>
+                </div>
+              </th>
+              <th>Description</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.id} onClick={() => userClickedHandler(user)}>
+                <th> {user.fullname} </th>
+                <td> {user.description} </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <div className="btn-container">
+        <button className="table-btn">View</button>
+        <button className="table-btn">Add</button>
+        <button className="table-btn">Delete</button>
+      </div>
+    </>
   );
 };
 
